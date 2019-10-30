a270000f2420bac7cdbce67efc64da88bbbcdb8b
Brandine Stackspear <bstacksp@su-b1.21-school.ru>
2019-10-30 21:53:51 +0300

    dodelat

---
+++ Assets/Scripts/ButtonTriggerScript.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonTriggerScript : MonoBehaviour
+{
+    public GameObject target;
+    public bool check = false;
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.CompareTag(("GUS")))
+        {
+            target.SetActive(check);
+        }
+    }
+}
